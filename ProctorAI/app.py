f4a5759d825bfff2f17bf5fd93d4c10ef4c5ff70
Nandini Sharma <146510069+NANDINISHARMA04@users.noreply.github.com>
2026-05-06 11:08:27 +0530

    Update app.py

--- ProctorAI/app.py
+++ ProctorAI/app.py
@@ -205,4 +205,4 @@ if __name__ == '__main__':
     print("  Open your browser at:  http://localhost:5000")
     print("═"*55 + "\n")
 
- socketio.run(app, host='0.0.0.0', port=5000, debug=False, use_reloader=False, allow_unsafe_werkzeug=True)
+socketio.run(app, host='0.0.0.0', port=5000, debug=False, use_reloader=False, allow_unsafe_werkzeug=True)
